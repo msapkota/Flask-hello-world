123c8628a42857598e6150f80af55d49b023fe97
Madhu Sapkota <msapkota@yahoo.com>
2016-01-16 18:13:58 -0600

    dynamic routes

--- app.py
+++ app.py
@@ -4,13 +4,44 @@ from flask import Flask
 #create application object
 app = Flask(__name__)
 
+
+#error handling
+app.config["DEBUG"] = True
+
 #use decorators to link the function to URL
 @app.route("/")
 @app.route("/hello")
 
 #define the view using function which returns string
 def hello_world():
-  return "Hello World from Madhu first Flask Test"
+  return "Hello World!!!"
+
+#dynamic routes
+@app.route("/integer/<int:value>")
+def int_type(value):
+  print value + 1
+  return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+  print value + 1
+  return "correct"
+
+@app.route("/path/<path:value>") 
+def path_type(value) :
+  print value 
+  return "correct"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+  return search_query
+
+@app.route("/name/<name>")
+def index(name):
+  if name.lower() == "madhu":
+    return "Hello, {}".format(name), 200
+  else:
+    return "Not Found", 404
 
 #start the development server using the run() method
 if __name__ == "__main__":
